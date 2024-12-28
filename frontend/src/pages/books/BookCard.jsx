bc7530749a6884bc682e35eabd8271bd5db861e6
tayyaba <tayyaba@gmail.com>
2024-12-29 00:30:47 +0500

    book pics

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { getimgurl } from '../../utils/getimgurl'
+
 import { FiShoppingCart } from 'react-icons/fi'
+
 const BookCard = ({book}) => {
   return (
     <div className=" rounded-lg transition-shadow duration-300">
@@ -7,20 +11,22 @@ const BookCard = ({book}) => {
     className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4"
   >
     <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
-      <a href="/">
+      <Link to={`/book._id`}>
         <img
-          src="assets/books/book-1.png"
+           src={getimgurl( book.coverImage )}
           alt=""
           className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
         />
-      </a>
+      </Link>
     </div>
 
     <div>
-      <a href="/"
-        ><h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
-          Book Title
-        </h3></a>
+      <Link to={`/book._id`}>
+        <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
+          {book.title}
+        </h3>
+      </Link>
+
       <p className="text-gray-600 mb-5">Book Description</p>
       <p className="font-medium mb-5">
         $80 <span className="line-through font-normal ml-2">$100</span>
