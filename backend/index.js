412e33627b6a5ffe438bf032ae9913ce715b2467
tayyaba <tayyaba@gmail.com>
2024-12-30 00:33:18 +0500

    Mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 
-//routes
-app.use('/',( _req,res ) => {
-    res.send("Book store server is running");
-})
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    app.use('/',( _req,res ) => {
+        res.send("Book store server is running");
+    });
+  }
+
+  main().then(()=> console.log("Mongodb connected successfully!")).catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
