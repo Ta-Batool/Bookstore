0730b1a44dd4ff9f8c625e54da01f91fa57feab8
INsha22 <221052@students.au.edu.pk>
2024-12-29 05:05:47 +0500

    Recomended

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Banner from "./Banner"
 import TopSellers from "./TopSellers"
+import Recomended from "./Recomended"
 
 const Home =()=>{
     return(
         <>
         <Banner/>
         <TopSellers/>
+        <Recomended/>
         </>
     )
 
