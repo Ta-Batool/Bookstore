ac029f5908fe67dbebff7d90af244498d7fb1fd8
tayyaba <tayyaba@gmail.com>
2024-12-30 22:29:06 +0500

    API inpoint book routes and controllers

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
+//middleware
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
+
+
+//routes
+const bookRoutes = require('./src/books/book.route')
+app.use("/api/books",bookRoutes)
 
 async function main() {
     await mongoose.connect(process.env.DB_URL);
